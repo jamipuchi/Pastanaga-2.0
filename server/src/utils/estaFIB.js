83f063b80061ebff70d55a3e9986bcbe12ea24a8
Arnau Buch <arnau.buch@est.fib.upc.edu>
2019-10-12 18:01:11 +0200

    Arregla comprovació dins/fora FIB

--- server/src/utils/estaFIB.js
+++ server/src/utils/estaFIB.js
@@ -1,10 +1,9 @@
-const x1 = "41.391700" // coordenades al voltant de la FIB
-const x2 = "41.385774"
-const y1 = "2.113015"
-const y2 = "2.111178"
+const center_x = "41.388554"
+const center_y = "2.112380"
+const radius = 300
 
 const estaFIB = (latitud, longitud) => {
-    if (latitud > x1 && latitud < x2 && longitud > y1 && longitud < y2) return true;
+    if ((latitud-center_x)^2 + (longitud - center_y)^2 <= radius^2) return true;
     else return false;
 }
 
