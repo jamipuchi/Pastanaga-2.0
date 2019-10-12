faed64da68f589a4ac16b6fec4d3193d38be524e
jamipuchi <jaumepuigturon4@gmail.com>
2019-10-12 05:45:39 +0200

    Acabat Power Ups

--- App/Pastanaga2.0/Components/PowerUps.js
+++ App/Pastanaga2.0/Components/PowerUps.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { AccordionList } from "accordion-collapse-react-native";
 import { Separator, Icon } from 'native-base';
+import { ScrollView } from 'react-native-gesture-handler';
 
 
 export default class PowerUps extends Component {
@@ -11,45 +12,66 @@ export default class PowerUps extends Component {
             list: [
                 {
                     id: 1,
-                    name: 'Getting Started',
-                    price: 'Getting Started',
+                    name: 'Brújola',
+                    price: 100,
                     description: 'Getting Started',
                     link: require("../assets/powerUps/1.png")
                 },
                 {
                     id: 2,
-                    name: 'Getting Started',
-                    price: 'Getting Started',
+                    name: 'Distància',
+                    price: 100,
                     description: 'Getting Started',
                     link: require("../assets/powerUps/2.png")
                 },
                 {
                     id: 3,
-                    name: 'Getting Started',
-                    price: 'Getting Started',
+                    name: 'Rang Matar',
+                    price: 100,
                     description: 'Getting Started',
                     link: require("../assets/powerUps/3.png")
                 },
                 {
                     id: 4,
-                    name: 'Getting Started',
-                    price: 'Getting Started',
+                    name: 'Avís',
+                    price: 100,
                     description: 'Getting Started',
                     link: require("../assets/powerUps/4.png")
                 },
                 {
                     id: 5,
-                    name: 'Getting Started',
-                    price: 'Getting Started',
+                    name: 'Invisibilitat',
+                    price: 100,
                     link: require("../assets/powerUps/5.png")
                 },
                 {
                     id: 6,
-                    name: 'Getting Started',
-                    price: 'Getting Started',
+                    name: 'Invulnerabilitat',
+                    price: 100,
                     description: 'Getting Started',
                     link: require("../assets/powerUps/6.png")
                 },
+                {
+                    id: 7,
+                    name: 'Està?',
+                    price: 100,
+                    description: 'Getting Started',
+                    link: require("../assets/powerUps/7.png")
+                },
+                {
+                    id: 8,
+                    name: 'Nom',
+                    price: 100,
+                    description: 'Getting Started',
+                    link: require("../assets/powerUps/8.png")
+                },
+                {
+                    id: 9,
+                    name: 'Foto',
+                    price: 100,
+                    description: 'Getting Started',
+                    link: require("../assets/powerUps/9.png")
+                }
             ],
         }
 
@@ -67,9 +89,9 @@ export default class PowerUps extends Component {
 
 
             }, (item.id == 1) ? { borderTopLeftRadius: 8, borderTopRightRadius: 8 } : {}
-                , (item.id == 6) ? { borderBottomLeftRadius: 8, borderBottomRightRadius: 8 } : {}]}>
+                , (item.id == 9) ? { borderBottomLeftRadius: 8, borderBottomRightRadius: 8 } : {}]}>
                 <Image
-                    style={{ width: 40, height: 40 }}
+                    style={{ width: 40, height: 40, resizeMode: 'contain' }}
                     source={item.link}
                 />
                 <Text
@@ -78,20 +100,19 @@ export default class PowerUps extends Component {
                         width: '50%',
                         textAlign: 'left',
                         textAlignVertical: 'center',
-                        backgroundColor: 'blue',
                         fontSize: 20,
                         overflow: 'hidden'
                     }}>
-                    {item.id}
+                    {item.name}
                 </Text>
                 <Text
                     style={{
                         width: '15%',
                         textAlign: 'right',
                         textAlignVertical: 'center',
-                        backgroundColor: 'red'
+                        fontSize: 15
                     }}>
-                    300
+                    300{" "}
                 </Text>
                 <Image
                     style={{ height: 20, width: 20, marginTop: 10 }}
@@ -110,7 +131,7 @@ export default class PowerUps extends Component {
     _body(item) {
         return (
             <View style={{ padding: 10, backgroundColor: 'white', width: '100%' }}>
-                <Text style={{ textAlign: 'center' }}>{item.body}</Text>
+                <Text style={{ textAlign: 'center' }}>{item.description}</Text>
                 <TouchableOpacity
                     activeOpacity={0.5}
                     style={{ height: 100, width: '100%' }}>
@@ -128,23 +149,29 @@ export default class PowerUps extends Component {
 
     render() {
         return (
-            <View style={{ width: '100%', height: '100%', paddingTop: '20%', paddingLeft: '5%', paddingRight: '5%', borderRadius: 10, backgroundColor: '#262626' }} >
+            <View style={{ width: '100%', height: '100%', paddingTop: '30%', paddingLeft: '5%', paddingRight: '5%', borderRadius: 10, backgroundColor: '#262626' }} >
+                <Text style={{
+                    marginLeft: '5%', fontSize: 30, color: '#6BDD5F',
+                    position: 'absolute', width: '100%', textAlign: 'center', top: '10%'
+                }}>
+                    Power Ups
+                </Text>
                 <TouchableOpacity
-                    style={{ width: 30 }}>
+                    onPress={() => this.props.navigation.goBack()}
+                    style={{ width: 30, position: 'absolute', left: '5%', top: '12%' }}>
                     <Image
                         source={require('../assets/back.png')}
                         style={{ height: 20, width: 20, resizeMode: 'contain' }}
                     ></Image>
                 </TouchableOpacity>
-                <Text style={{ fontSize: 30, color: '#6BDD5F' }}>
-                    Power Ups
-                    </Text>
-                <AccordionList
-                    style={{ marginTop: 100, width: '90%', marginLeft: '5%', borderRadius: 10 }}
-                    list={this.state.list}
-                    header={this._head}
-                    body={this._body}
-                />
+                    <ScrollView >
+                        <AccordionList
+                            style={{ width: '90%', marginLeft: '5%', borderRadius: 10 }}
+                            list={this.state.list}
+                            header={this._head}
+                            body={this._body}
+                        />
+                    </ScrollView>
 
             </View>
         );
