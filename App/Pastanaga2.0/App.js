e8be4efd6be14709181a6243a725b98e464a3bef
jamipuchi <jaumepuigturon4@gmail.com>
2019-10-12 03:22:19 +0200

    AuthFlow added

--- App/Pastanaga2.0/App.js
+++ App/Pastanaga2.0/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AsyncStorage } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack'
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import Login from './Login';
 import MainScreen from './Components/MainScreen';
+import Escanejar from './Escanejar';
 
-export default function App() {
-  return (
-    <MainScreen />
-  );
+
+class AuthLoadingScreen extends React.Component {
+  constructor() {
+    super();
+    this._bootstrapAsync();
+  }
+
+  // Fetch the token from storage then navigate to our appropriate place
+  _bootstrapAsync = async () => {
+    const userToken = await AsyncStorage.getItem('userToken');
+
+    // This will switch to the App screen or Auth screen and this loading
+    // screen will be unmounted and thrown away.
+
+    this.props.navigation.navigate(userToken ? 'App' : 'Auth');
+  };
+
+  render() {
+    return (
+      <View style={styles.containerb}>
+        <Text>Authentication screen</Text>
+      </View>
+    );
+  }
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,3 +41,30 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const AuthStack = createStackNavigator({ SignIn: Login }, {headerMode: 'none'});
+
+const AppStack = createStackNavigator(
+  {
+  MainScreen: {screen: MainScreen},
+  Escanejar: {screen: Escanejar},
+  },
+  {
+    initialRouteName: 'MainScreen',
+    headerMode: 'none',
+    navigationOptions: {
+        headerVisible: false,
+    }
+  }
+)
+
+export default createAppContainer(createSwitchNavigator(
+  {
+    AuthLoading: AuthLoadingScreen,
+    App: AppStack,
+    Auth: AuthStack,
+  },
+  {
+    initialRouteName: 'AuthLoading',
+  }
+));
