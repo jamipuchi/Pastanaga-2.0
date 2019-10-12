479c38202a6dee4c9905ff6e657799eb6ac6054a
Arnau Buch <arnau.buch@est.fib.upc.edu>
2019-10-12 22:26:07 +0200

    CORS

--- server/src/index.js
+++ server/src/index.js
@@ -78,4 +78,11 @@ server.express.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument)
 //     console.log('REST API Server listening  on http://localhost:3000')
 // })
 
+server.express.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 server.start({ port: 2368 }, () => console.log(`Server is running on http://localhost:2368`))
