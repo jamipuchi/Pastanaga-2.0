e738e8a078f298cf4ca527991ca93e793fb7af25
jamipuchi <jaumepuigturon4@gmail.com>
2019-10-13 05:05:52 +0200

    coses

--- App/Pastanaga2.0/Components/Perdut.js
+++ App/Pastanaga2.0/Components/Perdut.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import LottieView from 'lottie-react-native';
 
 export default class Perdut extends Component {
