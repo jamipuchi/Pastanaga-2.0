1ea46a96f397836e7487e1af9ff318e048e4d594
Arnau Buch <arnau.buch@est.fib.upc.edu>
2019-10-13 16:08:22 +0200

    Update estaFIB.js

--- server/src/utils/estaFIB.js
+++ server/src/utils/estaFIB.js
@@ -1,6 +1,6 @@
 const center_x = "41.388554"
 const center_y = "2.112380"
-const radius = 300
+const radius = 500
 
 const estaFIB = (latitud, longitud) => {
     if ((latitud-center_x)^2 + (longitud - center_y)^2 <= radius^2) return true;
